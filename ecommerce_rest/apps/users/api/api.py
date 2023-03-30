4f95f8e0a700500ef476c0c33ac2255513d07f88
Rocio Tournour <ro.tournour@gmail.com>
2023-03-30 15:13:40 -0300

    avances

--- ecommerce_rest/apps/users/api/api.py
+++ ecommerce_rest/apps/users/api/api.py
@@ -1,10 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
-class UserAPIView ():
-    def get(self, request):
+@api_view(['GET'])
+def user_api_view (request):
+
+    if request.method == 'GET':
         users = User.objects.all()
         users_serializer = UserSerializer(users, many = True)
-        return Response()
+        return Response(users_serializer.data)
